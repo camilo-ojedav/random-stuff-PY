3420c2fe25b1b5dc16dfc439acc348f7a79183b8
Camilo Ojeda <camilomiguel.ojeda@alumnos.ulagos.cl>
2023-07-18 22:44:46 -0400

    Update paginas.py
    Agregados comentarios en todo el codigo

--- paginas.py
+++ paginas.py
@@ -1,44 +1,40 @@
 import pyperclip as pyc
 import winsound as ws
 
+# 8     16      24      32      40      48      56       64     72      80
+# 88    96      104     112     120     128     136      144    152     160
+# 168   176     184     192     200     208     216      224    232     240   
 
-pagina = 23
+pagina = 8  #Numero de paginas del libro/documento
 
-c = 1
-
-arr = [2, 3, 6, 7, 4, 1, 8, 5]
-temp = []
-temp2 = [2, 3, 6, 7, 4, 1, 8, 5]
-mult = (pagina // 8) - 1
-resto = pagina % 8
-print("Resto = ", resto)
-
+arr = [2, 3, 6, 7, 4, 1, 8, 5]      # Array principal
+temp = []                           # Array temporal, en donde se guardan los numeros de una iteración del for
+output = [2, 3, 6, 7, 4, 1, 8, 5]   # Array de salida
+mult = (pagina // 8) - 1            # Se calcula el numero de iteraciones
+resto = pagina % 8                  
 
 for i in range(mult):
+    # Se agrupan los 8 digitos en temp, siguiendo el orden de arr
     for j in arr:
         operation = j + 8
         temp.append(operation)
         
-
+    # Se añaden a la salida
     for i in range(8):
-        temp2.append(temp[i])
-    arr = temp
-    temp = []
+        output.append(temp[i])
+    
+    arr = temp      # Se pasan los digitos temporales al array principal
+    temp = []       
+ 
+# if resto != 0:
+#     for i in range(resto):
+#         operation = arr[i] + 8
+#         output.append(operation)
 
-if resto != 0:
-    for i in range(resto):
-        operation = arr[i] + 8
-        temp.append(operation)
-
-print(temp2)
-texto = str(temp2)
+print(output)
+texto = str(output)
 texto = texto[1:-1]
 pyc.copy(texto)
 
 
-ws.Beep(2000, 500)  # Frecuencia de 2000 Hz durante medio segundo
-
-
-
-
-
+ws.Beep(2000, 100)  # Frecuencia de 2000 Hz durante 0.1 segundos
