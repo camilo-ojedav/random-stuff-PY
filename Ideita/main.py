7c627a5a7cde82c4c378d550504522759f8165b5
Camilo Ojeda <camilomiguel.ojeda@alumnos.ulagos.cl>
2023-11-27 21:07:41 -0300

    Add Selenium and json functions

--- Ideita/main.py
+++ Ideita/main.py
@@ -1,9 +1,30 @@
+from selenium import webdriver
 import json
+import time
 
-url = 'https://es.singlelogin.se'
 
-# acceder al usuario y contraseña de un json llamado credenciales
-with open('credenciales.json') as f:
-    credenciales = json.load(f)
-    email = credenciales['email']
-    password = credenciales['password']
+def abrir_navegador():
+    url = r'https://es.singlelogin.se'
+
+    # abrir el navegador
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+
+def abrir_json():
+
+    # acceder al usuario y contraseña de un json llamado credenciales
+    with open('credenciales.json', 'r') as f:
+        credenciales = json.load(f)
+        email = credenciales['email']
+        password = credenciales['password']
+
+        return email, password
+
+# espera 10 segundos antes de cerrar el navegador
+# time.sleep(10)
+
+
+# abrir_navegador()
+email, password = abrir_json()
+print(email, password)
